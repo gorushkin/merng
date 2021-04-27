ece782e740ed5200d979665a25acc433bd062cb4
gorushkin <gorushkin@gmail.com>
2021-04-27 20:54:35 +0300

    graphql!!!

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Grid, Image } from 'semantic-ui-react';
+// import graphql from 'graphql';
 
-const Home = () => <h1>Home</h1>;
+const Home = () => {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  console.log('data: ', data);
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <Image src='/images/wireframe/media-paragraph.png' />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+      }
+    }
+  }
+`;
 
 export default Home;
